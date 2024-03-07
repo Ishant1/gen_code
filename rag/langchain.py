876001bfaf35484bb9d956cc9abb8949be2228a1
Ishant Aggarwal <iagg1946@gmail.com>
2024-03-07 21:43:48 +0000

    Add new method to setup the api connection and a method to invoke langchain vertex ai

--- rag/langchain.py
+++ rag/langchain.py
@@ -1,1 +1,24 @@
 """File to implement the langchain pipeline"""
+from langchain_google_vertexai import VertexAI
+
+from utils.setup import setup_vertex_ai
+
+setup_vertex_ai(project="qwiklabs-gcp-04-7e74d632994e", location="us", key_json="key.json")
+
+
+def get_model_response(message: str, model_id: str= "gemini-pro") -> str:
+    """get the model response for a text generation model
+
+    :param message: The prompt to the model
+    :param model_id: The id/name of the model
+    :return: The response from the model
+    """
+    model = VertexAI(model_name=model_id)
+    return model.invoke(message)
+
+
+
+
+
+
+
---
+++ utils/setup.py
@@ -0,0 +1,30 @@
+"""File to help setup GCP connection and apis"""
+import os
+import vertexai
+
+
+def setup_vertex_ai(project: str=None, location: str=None, key_json: str = "key.json") -> None:
+    """Setup vertex-ai and gcp connection
+
+    This method uses the credential json file to build gcp connection
+    and then sets up vertex ai with given project and location.
+
+    Remember to enable vertex-ai and AI prediction api
+
+    :param project: ID of the associated project
+    :param location: location of vertex ai, as us or uk
+    :param key_json: Address of the json file
+    :return: None
+    """
+    if not os.path.exists(key_json):
+        ValueError("Please add authentication credentials json to root folder")
+
+    if not project:
+        project = os.getenv("PROJECT_ID")
+    if not location:
+        location = os.getenv("LOCATION")
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_json
+    vertexai.init(project=project, location=location)
+
+    
