b85a89b16925d4c5e9a1ab480e64da735f33aee1
Ishant Aggarwal <iagg1946@gmail.com>
2024-03-08 11:32:28 +0000

    Add github dataloader

--- Rag/Dataset/load_dataset.py
+++ Rag/Dataset/load_dataset.py
@@ -6,30 +6,44 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from git import Repo
 
 
-def get_git_code(github_link, repo_name):
-    repo_path = f"Rag/Dataset/{repo_name}"
-    Repo.clone_from(github_link, to_path=repo_path)
-    return repo_path
 
+class DataExtractor:
+    def __init__(self,githublink: str, data_path: str, languages: str=Language.PYTHON):
+        self.githublink = githublink
+        self.data_path = data_path
+        self.languages=languages
 
-def load_github_data_from_local(data_path, suffixes=[".py"], language=Language.PYTHON, parser_threshold=500):
-    # Load
-    loader = GenericLoader.from_filesystem(
-        data_path,
-        glob="**/*",
-        suffixes=suffixes,
-        exclude=["**/non-utf8-encoding.py"],
-        parser=LanguageParser(language=language, parser_threshold=parser_threshold),
-    )
-    return loader.load()
+        self.documents = None
 
+    def get_git_code(self):
+        Repo.clone_from(self.githublink, to_path=self.data_path)
 
-def split_documents(documents, language=Language.PYTHON, chunk_size=2000, chunk_overlap=200):
-    code_splitter = RecursiveCharacterTextSplitter.from_language(
-        language=language, chunk_size=chunk_size, chunk_overlap=chunk_overlap
+    def load_github_data_from_local(self, suffixes=[".py"], parser_threshold=500):
+        # Load
+        loader = GenericLoader.from_filesystem(
+            self.data_path,
+            glob="**/*",
+            suffixes=suffixes,
+            exclude=["**/non-utf8-encoding.py"],
+            parser=LanguageParser(language=self.languages, parser_threshold=parser_threshold),
         )
-    split_documents = code_splitter.split_documents(documents)
-    return split_documents
 
+        self.documents = loader.load() 
 
+    def split_documents(self, documents, chunk_size=2000, chunk_overlap=200):
+        code_splitter = RecursiveCharacterTextSplitter.from_language(
+            language=self.languages, chunk_size=chunk_size, chunk_overlap=chunk_overlap
+            )
+        split_documents = code_splitter.split_documents(documents)
+        return split_documents
 
+    def get_all_text(self):
+        page_based_text = [doc_.page_content for doc_ in self.documents]
+        file_names = [doc_.metadata.get("source") for doc_ in self.documents]
+
+        combined_text = ""
+        for page_text_, filename_ in zip(page_based_text, file_names):
+            file_text = f"{filename_}\n {page_text_}"
+            combined_text = f"{combined_text}\n\n{file_text}"
+
+        return combined_text
