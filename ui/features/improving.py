932664866ad517cbeba4120cf1f5186132ed4ca1
Garvit Mathur <gaganmathur99@gmail.com>
2024-03-08 14:48:29 +0000

    unit test input

--- ui/features/improving.py
+++ ui/features/improving.py
@@ -9,5 +9,8 @@ def get_model_output():
 
 def improving_page():
     codebase = st.session_state['dataExtractor'].get_all_text()
-    output = get_model_response(get_prompt_for_testing(codebase))
-    st.write(output)
+    input_text = st.text_input("Enter a file name to create unit test: ", key="input file")
+    if input_text:
+        # @TODO add prompt for 
+        unit_test = get_model_response(get_prompt_for_unit_test(codebase, input_text))
+        st.write(unit_test)
