5f9abc7ed64e948f53d59145ed9035d266fec72c
Ishant Aggarwal <iagg1946@gmail.com>
2024-03-08 14:21:27 +0000

    Adding new methods to call vertex api

--- ui/features/learning.py
+++ ui/features/learning.py
@@ -4,10 +4,6 @@ from Rag.Models.prompts import get_prompt_for_learning
 from Rag.langchain import get_model_response
 
 
-def get_model_output():
-    return "Hello World"
-
-
 def learning_page():
     codebase = st.session_state['dataExtractor'].get_all_text()
     output = get_model_response(get_prompt_for_learning(codebase))
